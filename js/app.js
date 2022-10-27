be708b05a88e52a9ea92417794960338b9b93d7c
cristianbyte <charlsmaritz@gmail.com>
2022-10-27 03:55:31 -0500

    API POKEMON lista: buscar por id, borrar, monstrar estadisticas, etc

--- js/app.js
+++ js/app.js
@@ -5,13 +5,40 @@ function move(){
 }
 pokedexOut.addEventListener('click', move)
 
-function capitalize(word) {return word[0].toUpperCase() + word.slice(1);}
+const tiposTranslate = {
+steel:'acero', water: 'agua', bug: 'bicho',dragon:'dragón',
+electric:'eléctrico', ghost:'fantasma',fire:'fuego',fairy:'hada',
+ice:'hielo',fighthing:'lucha',normal:'normal',grass:'planta',
+psychic:'psíquico',rock:'roca',dark:'siniestro',ground:'tierra',
+poison:'veneno',flying:'volador'}
 
 const pokedex__window = document.getElementsByClassName('dataPokemon')
-let pokedexPrincipalImg = document.getElementsByTagName('img')
 const tecladoPokedex = document.getElementsByClassName('pokedex__2-buttons')[0]
-let pokedex__window2Info = document.getElementsByClassName('pokedex__2-window--down')[0]
-let numberForSearching = ''
+const search = document.getElementsByClassName('pokedex__2-buttons-search')[0]
+const borrar = document.getElementsByClassName('pokedex__2-buttons-delete')[0]
+let pokedexPrincipalImg = document.getElementsByTagName('img')
+let pokedex__window2Info = document.getElementsByClassName('pokedex__2-window--data')[0]
+let pokedex__window2Info2 = document.getElementsByClassName('pokedex__2-window--data2')[0]
+let pokedex__window2InfoNumber = document.getElementsByClassName('pokedex__2-window--number')[0]
+let numberForSearch = '003'
+
+function searchEvent(){
+    if( 0 < numberForSearch && numberForSearch < 900 ){
+        loadPokemon(numberForSearch)
+    }
+    else{
+        console.log('error')
+    }
+}
+
+function deleteNumber(){
+    numberForSearch = '0'
+    pokedex__window2InfoNumber.innerHTML = numberForSearch
+}
+
+
+borrar.addEventListener('click', deleteNumber )
+search.addEventListener('click', searchEvent )
 
 tecladoPokedex.addEventListener('click',(e)=>{
     datos = {
@@ -20,49 +47,62 @@ tecladoPokedex.addEventListener('click',(e)=>{
         x_pointer:e.target.offsetLeft,
         y_pointer:e.target.offsetTop,
     }
+    numberForSearch +=''
     if(datos.y_pointer == 0){
-        numberForSearching += Math.trunc(datos.x_pointer/datos.w_element_s)
+        numberForSearch += Math.trunc(datos.x_pointer/datos.w_element_s)
     }
     else{
-        numberForSearching += Math.trunc(datos.x_pointer/datos.w_element_s) + 5
+        numberForSearch += Math.trunc(datos.x_pointer/datos.w_element_s) + 5
     }
-    if(numberForSearching.length == 4){
-        numberForSearching = numberForSearching.substring(1)
+    if(numberForSearch.length == 4){
+        numberForSearch = numberForSearch.substring(1)
     }
-    pokedex__window2Info.innerHTML = numberForSearching
+    pokedex__window2InfoNumber.innerHTML = numberForSearch
+console.log(pokedex__window2InfoNumber );
 })
 
 let pokeData = {}
-let pokePrincipalData = {}
 let pokeDataTemp = ''
+pokedex__window2InfoNumber.innerHTML = numberForSearch
+function cleanData(){
+    pokeDataTemp = ''
+    pokedex__window[0].innerHTML = ''
+    pokedex__window2Info.innerHTML = ''
+    pokedex__window2Info2.innerHTML = ''
+}
 
-async function loadPokemon(num=27){
-    pokePrincipalData = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
-    pokePrincipalData = await pokePrincipalData.json()
-    pokedex__window[0].innerHTML= `${capitalize(pokePrincipalData.name)} <br><br>Habilidades:<br>`;
-    pokePrincipalData.abilities.forEach((e)=>{
-        pokeDataTemp += `${capitalize(e.ability.name)} <br>`
-        console.log(pokePrincipalData);
+
+async function loadPokemon(num){
+    cleanData()
+    pokeData = await fetch(`https://pokeapi.co/api/v2/pokemon/${+num}`)
+    pokeData = await pokeData.json()
+    
+    pokedex__window[0].innerHTML= `${pokeData.name} <br><br>Habilidades:<br>`;
+    
+    pokeData.abilities.forEach((e)=>{
+        pokeDataTemp += `${e.ability.name} <br>`
     })
-    pokeDataTemp += 'Tipos: <br>'
-    pokePrincipalData.types.forEach((e)=>{
-        pokeDataTemp += `${capitalize(e.type.name)} <br>`
+    pokeDataTemp += 'Tipo: <br>'
+    pokeData.types.forEach((e)=>{
+        pokeDataTemp += `${tiposTranslate[e.type.name]} <br>`
     })
     pokedex__window[0].innerHTML+=`${pokeDataTemp} `
-    pokedexPrincipalImg[0].setAttribute('src',`${pokePrincipalData.sprites.other.dream_world.front_default}`)
+    pokedex__window2InfoNumber.style.backgroundImage =`url(${pokeData.sprites.front_default})`
+
+    pokeDataTemp = pokeData.sprites.other.dream_world.front_default ?? pokeData.sprites.front_default
+    pokedexPrincipalImg[0].setAttribute('src',`${pokeDataTemp}`)
+
+    pokeData.stats.forEach((e)=>{
+        pokedex__window2Info.innerHTML += `${e.stat.name}:<br>`
+        pokedex__window2Info2.innerHTML += `${e.base_stat}<br>`
+    })
+    
+    
 }
-loadPokemon()
+loadPokemon(numberForSearch)
 
 
 // .sprites.front_default
 // weight
 
 // stats [] base_stat stat.name
-
-const tiposTranslate = {steel:'acero', 
-water: 'agua', bug: 'bicho', dragon:'dragón',
-electric:'eléctrico', ghost:'fantasma',fire:'fuego',
-fairy:'hada',ice:'hielo',fighthing:'lucha',
-normal:'normal',grass:'planta',psychic:'psíquico',
-rock:'roca',dark:'siniestro',ground:'tierra',
-poison:'veneno',flying:'volador'}
